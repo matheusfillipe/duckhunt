b8012d9d8e6886bafd19fc855fc5dfb68ba53493
loudercake <jxzk@protonmail.com>
2023-06-30 16:22:34 -0300

    add fps camera moving

---
+++ Player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody3D
+
+const mouseSensibility = 600
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var mouse_relative_x = 0
+var mouse_relative_y = 0
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation.y -= event.relative.x / mouseSensibility
+		$Camera3D.rotation.x -= event.relative.y / mouseSensibility
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, deg_to_rad(-90), deg_to_rad(90) )
+		mouse_relative_x = clamp(event.relative.x, -50, 50)
+		mouse_relative_y = clamp(event.relative.y, -50, 10)
