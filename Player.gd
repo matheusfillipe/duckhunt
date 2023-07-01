c6aa58c224e4ce8d7f74756c0cf9ce05e6fa0db6
Matheus Fillipe <matheusfillipeag@gmail.com>
2023-07-02 00:07:22 +0200

    remove unused variables

--- Player.gd
+++ Player.gd
@@ -2,9 +2,6 @@ extends CharacterBody3D
 
 const mouseSensibility = 600
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var mouse_relative_x = 0
