efe60b25f982bee76cac6df30d664bb854d61c2a
Matheus Fillipe <matheusfillipeag@gmail.com>
2023-07-01 23:52:06 +0200

    Format script

--- Player.gd
+++ Player.gd
@@ -9,12 +9,16 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var mouse_relative_x = 0
 var mouse_relative_y = 0
+
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotation.y -= event.relative.x / mouseSensibility
 		$Camera3D.rotation.x -= event.relative.y / mouseSensibility
-		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, deg_to_rad(-90), deg_to_rad(90) )
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 		mouse_relative_x = clamp(event.relative.x, -50, 50)
 		mouse_relative_y = clamp(event.relative.y, -50, 10)
