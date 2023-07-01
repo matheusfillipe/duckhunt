93dd78f59059165dee431803f67f55c8f3a4df22
Matheus Fillipe <matheusfillipeag@gmail.com>
2023-07-02 01:39:07 +0200

    Added crosshair and improve camera shaking

--- Camera.gd
+++ Camera.gd
@@ -1,11 +1,10 @@
 extends Camera3D
 
 @onready var noise = FastNoiseLite.new()
+@onready var center_rotation: Vector3 = rotation
 
-@export_range(0, 2) var amplitude: float = 0.5
-@export_range(0, 2) var trauma: float = 0.8
+@export_range(0, 1) var shake_amplitude: float = 0.25
 
-var trauma_power = 2
 var NOISE_SPEED = 50
 var _noise_y = 0
 
@@ -14,21 +13,16 @@ func _ready():
 	randomize()
 	noise.seed = randi()
 	noise.noise_type = FastNoiseLite.TYPE_PERLIN
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
-
-func _shake():
-	var amount = pow(trauma, trauma_power)
-	position = (
-		Vector3(
-			noise.get_noise_2d(noise.seed, _noise_y),
-			noise.get_noise_2d(noise.seed*2, _noise_y),
-			0,
-		)
-		* amplitude * amount
-	)
 
 
 func _physics_process(delta: float):
 	_noise_y += delta * NOISE_SPEED
-	_shake()
+	var shake_vec = (
+		Vector3(
+			noise.get_noise_2d(noise.seed, _noise_y),
+			noise.get_noise_2d(noise.seed * 2, _noise_y),
+			0,
+		)
+		* shake_amplitude
+	)
+	rotation = center_rotation + shake_vec
--- Player.gd
+++ Player.gd
@@ -12,10 +12,10 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
-func _input(event):
+func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotation.y -= event.relative.x / mouseSensibility
-		$Camera3D.rotation.x -= event.relative.y / mouseSensibility
-		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		$Camera3D.center_rotation.x -= event.relative.y / mouseSensibility
+		$Camera3D.center_rotation.y = clamp($Camera3D.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 		mouse_relative_x = clamp(event.relative.x, -50, 50)
 		mouse_relative_y = clamp(event.relative.y, -50, 10)
